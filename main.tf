b02fe4db87627bbfe8d0ca32ad0c750a06319e0b
PL00618456 <65302050+PL00618456@users.noreply.github.com>
2022-05-16 18:14:09 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,13 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+resource "aws_instance" "web" {
+  ami             = data.aws_ami.amazon_linux.id
+  instance_type   = "t2.micro"
+  key_name        = var.key_name
+  security_groups = [aws_security_group.jenkins_sg.name]
+  user_data       = "${file("install_jenkins.sh")}"
+  tags = {
+    Name = "Jenkins"
+  }
+}
